a1df97b65e8138e957ded4aa5627b749324063bf
viswarepo <72087690+viswarepo@users.noreply.github.com>
2025-10-27 11:58:26 +0530

    Update TestServiceApplication.java

--- src/main/java/com/app/domain/TestServiceApplication.java
+++ src/main/java/com/app/domain/TestServiceApplication.java
@@ -3,11 +3,8 @@ package com.app.domain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class TestServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestServiceApplication.class, args);
-	}
 
 }
